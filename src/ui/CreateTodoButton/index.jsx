911945c154faee3a0c20652c97a2068d156dfbcc
santino <santinofernandez67@gmail.com>
2025-12-03 19:26:19 -0300

    cambios

---
+++ src/ui/CreateTodoButton/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './CreateTodoButton.css';
+
+function CreateTodoButton(props) {
+ 
+  return (
+    <button
+      className="CreateTodoButton"
+      onClick={props.onClick}
+    >
+      +
+    </button>
+  );
+}
+
+export { CreateTodoButton };
---
+++ src/ui/TodoForm/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './TodoForm.css';
+
+
+function TodoForm(props) {
+  const navigate = useNavigate();
+  const [newTodoValue, setNewTodoValue] = React.useState(props.defaultTodoText || '');
+  const onChange = (event) => {
+    
+    setNewTodoValue(event.target.value);
+  };
+  const onCancel = () => {
+    navigate('/')
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    props.submitEvent(newTodoValue);
+    navigate('/');
+   
+    
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label>{props.label}</label>
+      <textarea
+        value={newTodoValue}
+        onChange={onChange}
+        placeholder="Cortar la cebolla oara el almuerzo"
+      />
+      <div className="TodoForm-buttonContainer">
+        <button
+          type="button"
+          className="TodoForm-button TodoForm-button--cancel"
+          onClick={onCancel}
+          >
+          Cancelar
+        </button>
+        <button
+          type="submit"
+          className="TodoForm-button TodoForm-button--add"
+        >
+          {props.submitText}
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export { TodoForm };
